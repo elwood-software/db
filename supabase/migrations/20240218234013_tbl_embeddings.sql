7f1fcc55d31859494eb8613e9a620e49027d1c3f
Travis Kuhl <travis.kuhl@gmail.com>
2024-06-02 12:14:13 -0700

    release

--- supabase/migrations/20240218234013_tbl_embeddings.sql
+++ supabase/migrations/20240218234013_tbl_embeddings.sql
@@ -3,16 +3,15 @@
 create table if not exists elwood.embedding (
   "instance_id" uuid not null default '00000000-0000-0000-0000-000000000000',
   "id" uuid not null default extensions.uuid_generate_v4(),
-  "bucket_id" text null,
   "object_id" uuid null,
-  "embedding" extensions.vector(384),
+  "chunk_id" text not null,
+  "embedding" extensions.vector(1536),
   "summary" text null,
+  "content" text null,
   "search_text" tsvector null,
   "created_at" timestamptz default now(),
   "updated_at" timestamptz default now(),
 
-  constraint "elwood_follow_bucket_id"
-    foreign key ("bucket_id") references "storage"."buckets"("id"),
   constraint "elwood_follow_object_id"
     foreign key ("object_id") references "storage"."objects"("id"),
   primary key ("id")
@@ -21,8 +20,8 @@ create table if not exists elwood.embedding (
 
 create unique index if not exists elwood_idx_embedding_object on elwood.embedding (
   "instance_id",
-  "bucket_id",
-  "object_id"
+  "object_id",
+  "chunk_id"
 );
 
 create index if not exists elwood_idx_embedding_search_text on elwood.embedding using gin("search_text");
