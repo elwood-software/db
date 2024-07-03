3edb8a161f0a813e75f1d5fe35e69a9a320ea70f
Travis Kuhl <travis.kuhl@gmail.com>
2024-07-02 20:49:18 -0700

    move to deno

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env -S deno run --allow-run --allow-net --allow-env --allow-read --allow-write
 
+// deno-lint-ignore-file
+
 /**
  * release.ts
  *
@@ -19,6 +21,22 @@ import { fileURLToPath } from "node:url";
 import { loadSync } from "https://deno.land/std@0.220.1/dotenv/mod.ts";
 import * as semver from "https://deno.land/std@0.224.0/semver/mod.ts";
 import { parseArgs } from "https://deno.land/std@0.224.0/cli/mod.ts";
+import * as path from "node:path";
+import { promises as fs } from "node:fs";
+import {
+  DatabaseConnection,
+  FileMigrationProvider,
+  Generated,
+  Kysely,
+  Migrator,
+  PostgresAdapter,
+  PostgresDialect,
+  PostgresIntrospector,
+  PostgresQueryCompiler,
+  QueryResult,
+} from "npm:kysely";
+
+import { DummyDriver } from "./db.ts";
 
 const args = parseArgs(Deno.args, {
   boolean: ["overwrite"],
@@ -35,14 +53,22 @@ loadSync({
 });
 
 const rootDir = join(__dirname, "..");
-const migrationsDir = join(rootDir, "supabase/migrations");
+const migrationsDir = join(rootDir, "src/migrations");
 const versionsDir = join(rootDir, "versions");
+
+const latestVersion = JSON.parse(
+  await Deno.readTextFile(join(versionsDir, "latest.json")),
+) as {
+  version: string;
+  sql: string;
+  migrations: string[];
+};
 
 const controlData = await Deno.readTextFile(join(rootDir, "elwood.control"));
 const defaultVersion =
   Array.from(controlData.match(/default_version = '(.*)'/) ?? [])[1];
 let nextVersion = semver.format(
-  semver.increment(semver.parse(defaultVersion), "patch"),
+  semver.increment(semver.parse(defaultVersion), "minor"),
 );
 
 if (args.overwrite === true) {
@@ -61,19 +87,54 @@ if (args.overwrite === false) {
   }
 }
 
-const sqlFiles: string[] = [];
-const toSquash: string[] = [];
+const driver = new DummyDriver();
+const db = new Kysely<any>({
+  dialect: {
+    createAdapter() {
+      return new PostgresAdapter();
+    },
+    createDriver() {
+      // You need a driver to be able to execute queries. In this example
+      // we use the dummy driver that never does anything.
+      return driver;
+    },
+    createIntrospector(db: Kysely<unknown>) {
+      return new PostgresIntrospector(db);
+    },
+    createQueryCompiler() {
+      return new PostgresQueryCompiler();
+    },
+  },
+}).withSchema("elwood");
 
-for await (const dirEntry of Deno.readDir(migrationsDir)) {
-  if (dirEntry.isFile && !ignoreMigrations.some((r) => r.test(dirEntry.name))) {
-    sqlFiles.push(dirEntry.name);
+const migrations = new FileMigrationProvider({
+  fs,
+  path,
+  // This needs to be an absolute path.
+  migrationFolder: path.join(__dirname, "../src/migrations"),
+});
+
+const migrationNames: string[] = [];
+const sql: string[] = [];
+
+for (
+  const [name, migration] of Object.entries(await migrations.getMigrations())
+) {
+  migrationNames.push(name);
+
+  if (latestVersion.migrations.includes(name)) {
+    continue;
   }
+
+  driver.reset();
+  await migration.up(db);
+
+  sql.push(`-- src:${name}\n${driver.sql.join("\n")}\n--\n\n`);
 }
 
-sqlFiles.sort();
-
-for (const sqlFileName of sqlFiles) {
-  toSquash.push(await Deno.readTextFile(join(migrationsDir, sqlFileName)));
+if (sql.length === 0) {
+  console.error("No migrations from last version to this version");
+  Deno.exit(1);
 }
 
 // write the squashed migrations to the version file
@@ -82,7 +143,7 @@ await Deno.writeFile(
     rootDir,
     versionFileTemplate.replace("${version}", nextVersion),
   ),
-  new TextEncoder().encode(toSquash.join("\n")),
+  new TextEncoder().encode(sql.join("\n")),
 );
 
 // update the control file with the new version
@@ -96,6 +157,12 @@ await Deno.writeFile(
   ),
 );
 
+const versionData = {
+  version: nextVersion,
+  sql: sql.join("\n"),
+  migrations: migrationNames,
+};
+
 // write the latest version file to the versions dir
 await Deno.writeFile(
   join(
@@ -104,10 +171,7 @@ await Deno.writeFile(
   ),
   new TextEncoder().encode(
     JSON.stringify(
-      {
-        version: nextVersion,
-        sql: toSquash.join("\n"),
-      },
+      versionData,
       null,
       2,
     ),
@@ -122,10 +186,7 @@ await Deno.writeFile(
   ),
   new TextEncoder().encode(
     JSON.stringify(
-      {
-        version: nextVersion,
-        sql: toSquash.join("\n"),
-      },
+      versionData,
       null,
       2,
     ),
